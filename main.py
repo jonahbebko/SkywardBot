036ad524c2b97992228f1f9f5ada2e7ddd7b66aa
Jonah Bebko <jonahbebko@outlook.com>
2022-09-27 22:56:51 -0400

    placeholder commands

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ sends a message in dms to everyone with the pinged role
 sends a list of caster availability set by /setcasterinfo
 
 /setcasterinfo <message> (admin only)
-sets the message given by /setcasterinfo
+sets the message given by /casterinfo
 
 /report <week> <ID> <score-score> <ID> (dms only)
 used to report match, sends the info to a designated channel or gsheet
@@ -40,6 +40,26 @@ async def hello(ctx):
 @bot.slash_command(name = "benjamin", description = "benjamin")
 async def hello(ctx):
     await ctx.respond("benjamin :flushed:")
+
+@bot.slash_command(name = "dm", description = "Sends a message in dms to everyone with the pinged role")
+async def hello(ctx):
+    await ctx.respond("this shit dont work yet, give bot man joner some time")
+
+@bot.slash_command(name = "casterinfo", description = "Sends a list of caster availability set by /setcasterinfo")
+async def hello(ctx):
+    await ctx.respond("this shit dont work yet, give bot man joner some time")
+
+@bot.slash_command(name = "setcasterinfo", description = "Sets the message given by /casterinfo")
+async def hello(ctx):
+    await ctx.respond("this shit dont work yet, give bot man joner some time")
+
+@bot.slash_command(name = "report", description = "Used to report match, sends the info to a designated channel")
+async def hello(ctx):
+    await ctx.respond("this shit dont work yet, give bot man joner some time")
+
+@bot.slash_command(name = "requestcaster", description = "Used to request a caster, said caster is then pinged in a designated channel with the info")
+async def hello(ctx):
+    await ctx.respond("this shit dont work yet, give bot man joner some time")
 
 with open("token.txt") as token:
     bot.run(token.read())
