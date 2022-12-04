f6acc44a44179ba62b6562ef71aaf8abf8c3eaed
Jonah Bebko <jonahbebko@outlook.com>
2022-12-03 22:04:31 -0500

    /ban and /banlist fix

--- main.py
+++ main.py
@@ -113,7 +113,7 @@ async def banlist(ctx):
             await ctx.respond(embed=discord.Embed(
                 title="Banned Users/Groups",
                 color=0xFF0000,
-                description=[f'`{i}`' for i in banl]
+                description=[i for i in banl]
             ))
     else:
         await ctx.respond(embed=discord.Embed(
