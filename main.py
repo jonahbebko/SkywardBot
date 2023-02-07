cdae60dc47dfce2f569f8bd255bbaa0174015f9c
jonahbebko <jonahbebko@outlook.com>
2023-02-07 18:40:49 -0500

    removed caster stuffs

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 import json
-import discord
-import re
-import asyncio
+import discord #py-cord, not discord.py
 
-LOG="""Last updated: February 5, 2023
-- Added optional ballchasers link to /report and /forfeit
-- Corrected some colors to Skyward Orange
-- Removed unauthorized admin command usage logging because I don't really care
-- Fixed a silly goofy error that broke the entire bot sometimes
+LOG="""Last updated: February 7, 2023
+- Removed commented-out functions, because version control exists
+- Changed /casterinfo to link to UrNextUp
+- Removed /requestcaster
 """
 
 ROLES = {
@@ -54,45 +51,6 @@ async def on_message(ctx):
             user = bot.get_user(int(ctx.content.split()[1]))
         await user.send(' '.join(ctx.content.split()[2:]))
         await ctx.channel.send(f"Sent: {ctx.content.split()[1]} - {' '.join(ctx.content.split()[2:])}")
-
-"""
-@bot.slash_command(name="banlist", description="List of banned users (admin only)")
-async def banlist(ctx):
-    if ctx.author.guild_permissions.administrator:
-        with open("bans.txt", "r") as f:
-            await ctx.respond(embed=discord.Embed(
-                title="Banned Users/Groups",
-                color=0xFF0000,
-                description=f.read()
-            ))
-    else:
-        await ctx.respond(embed=discord.Embed(
-            title="SkywardBot - Error",
-            description="You must be an administrator to use this command.",
-            color=0xFF0000
-            )
-        )
-
-@bot.slash_command(name="ban", description="Add user to ban list (admin only)", options=[
-    discord.Option(name="msg", description="ID/group to ban, or a message", required=True, type=str),
-    discord.Option(name="reason", description="Reason for ban", required=False, type=str)
-])
-async def ban(ctx, msg, reason:None):
-    if ctx.author.guild_permissions.administrator:
-        with open("bans.txt", "a") as f:
-            if reason:
-                f.write(f"{msg} - {reason}")
-            else:
-                f.write(f"{msg}")
-        await ctx.respond(f"Banned **{msg}** for **{reason}**")
-    else:
-        await ctx.respond(embed=discord.Embed(
-            title="SkywardBot - Error",
-            description="You must be an administrator to use this command.",
-            color=0xFF0000
-            )
-        )
-"""
 
 @bot.slash_command(name="help", description="Show list of commands.")
 async def help(ctx):
@@ -211,7 +169,7 @@ async def casterinfo(ctx):
         await ctx.respond(embed=discord.Embed(
             color=0xFF9179,
             title="Caster Info",
-            description="<:dot:1031708752140832768> You can see caster availability [here!](https://docs.google.com/spreadsheets/d/1YfXo1ehAI8GDIiwG6dI09_In2VbxX7TjwBLA0Lgs430/edit?usp=sharing)"
+            description="<:dot:1031708752140832768> You can see UNU caster availability [here!](https://urnextup.gg)"
         ))
     else:
         await ctx.respond(embed=discord.Embed(
@@ -379,85 +337,6 @@ async def forfeit(ctx, league, gamemode, week, team_one_tag, team_two_tag, fftyp
     await ctx.respond(embed=discord.Embed(
         title="SkywardBot - Info",
         description="Report sent.",
-        color=0xFF9179
-    ))
-
-@bot.slash_command(name="requestcaster", description="Used to request a caster, said caster is then pinged in a designated channel with the info.", options=[
-    discord.Option(name="day", description="Day of the match", type=str),
-    discord.Option(name="time", description="Time of the match. MUST BE IN EASTERN TIME.", type=str),
-])
-async def requestcaster(ctx, day, time):
-
-    if not (ctx.channel.type == discord.ChannelType.private or ctx.channel.id == 1031781423864090664):
-        await ctx.respond(embed=discord.Embed(
-            title="SkywardBot - Error",
-            description="This is a DMs-only command.",
-            color=0xFF0000
-        )); return
-
-    # check if day in MM/DD format using regex
-    if not re.match(r"^(0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$", day):
-        await ctx.respond(embed=discord.Embed(
-            title="SkywardBot - Error",
-            description=f"**Error** in parameter `day`, given '{day}'\nDay must be a valid day, and in the format `MM/DD`",
-            color=0xFF0000
-        )); return
-    
-    await ctx.respond(
-        embed=discord.Embed(
-            description="**PLEASE CHECK CASTER SCHEDULES WITH /casterinfo BEFORE REQUESTING A CASTER**\n" + \
-            "Respond 'yes' to confirm you've checked the schedule."
-        )
-    )
-
-    try:
-        msg = await bot.wait_for("message", check=lambda m: m.author == ctx.author, timeout=20)
-    except asyncio.TimeoutError:
-        await ctx.respond(embed=discord.Embed(
-            title="SkywardBot - Error",
-            description="Confirmation timed out after 20 seconds, please try again.",
-            color=0xFF0000
-        )); return
-    
-    if msg.content.lower() != "yes":
-        await ctx.respond(embed=discord.Embed(
-            title="SkywardBot - Error",
-            description="Confirmation failed, please try again.",
-            color=0xFF0000
-        )); return
-    
-    await ctx.send(
-        "Please enter the number corresponding to the caster you would like to request:\n",
-        embed=discord.Embed(
-            color=0xFF9179,
-            title="Caster Request Numbers",
-            description="\n".join([f"{i+1}. {caster}" for i, caster in enumerate(ids)])
-        )
-    )
-    try:
-        msg = await bot.wait_for("message", check=lambda m: m.author == ctx.author, timeout=20)
-    except asyncio.TimeoutError:
-        await ctx.respond(embed=discord.Embed(
-            title="SkywardBot - Error",
-            description="Request timed out after 20 seconds, please try again.",
-            color=0xFF0000
-        )); return
-    
-    if msg.content == "joner":
-        await bot.get_channel(1025196891794853888).send(f"<@{ctx.author.id}>\nYou can't request joner, he's not a caster. But this test works, so that's good.\nparameters: `{day}`, `{time}`")
-        return
-    
-    await bot.get_channel(1025196891794853888).send(
-        f"<@{[i for i in ids.values()][int(msg.content)-1]}>",
-        embed=discord.Embed(
-            title="Caster Request",
-            description=f"\n{ctx.author} has requested a caster for **{day}** at **{time}**"
-        )
-    )
-
-    await ctx.respond(embed=discord.Embed(
-        title="SkywardBot - Info",
-        description="Caster request sent.",
         color=0xFF9179
     ))
 
