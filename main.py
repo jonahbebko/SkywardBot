46bb8b0def23987f3d961ff5edf343366772d55b
jonahbebko <jonahbebko@outlook.com>
2023-02-23 20:57:57 -0500

    kwargs gotta work

--- main.py
+++ main.py
@@ -40,9 +40,9 @@ async def error_embed(ctx, e):
             await member.send(f"SOMETHING WENT WRONG FUCKFACE\n{e}")
 
 def catch_errors(func):
-    async def wrapper(ctx, *args):
+    async def wrapper(ctx, **kwargs):
         try:
-            return await func(ctx, *args)
+            return await func(ctx, **kwargs)
         except Exception as e:
             print(e)
             await error_embed(ctx, e)
